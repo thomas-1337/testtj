47401460df984a12fb222899c4494c2f332aed92
thomas-1337 <thomas.jungmann@web.de>
2020-03-04 10:34:03 +0100

    initial

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,26 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        Person p = new Person();
+
+        p.name = "Max";
+        p.nr   = 123;
+        p.printPerson();
+
+        Person.test();
     }
+}
+
+class Person {
+    String name;
+    int nr;
+
+    public void printPerson () {
+        System.out.println("My name is: " + name);
+        System.out.println("My number is: " + nr);
+    }
+
+    public static void test() {
+        System.out.println("I'm static");
+    }
+
 }
