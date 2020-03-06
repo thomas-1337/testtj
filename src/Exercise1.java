7c19087dd22841f9bd592efaf48d160fba23d73e
thomas-1337 <thomas.jungmann@web.de>
2020-03-06 11:33:04 +0100

    coding

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,5 +1,34 @@
+import java.io.*;
+
 public class Exercise1 {
     public static void main(String[] args) {
+        File file = new File("test.txt");
+        File file_target = new File("test_copy.txt");
+
+        System.out.println(file.getAbsoluteFile());
+
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader bfr = new BufferedReader(fr);
+
+            FileWriter fw = new FileWriter(file_target);
+            PrintWriter pw = new PrintWriter(fw);
+
+            String line;
+            while ((line = bfr.readLine()) != null) {
+                pw.println(line.toUpperCase());
+                pw.flush();
+            }
+
+            bfr.close();
+            fr.close();
+
+            pw.close();
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
